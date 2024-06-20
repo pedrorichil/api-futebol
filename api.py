68cc6f6ba6514d358d7031b6938ba41f521def94
Pedro Richil <pedrorichillbia@gmail.com>
2024-06-20 17:40:27 -0300

    main

---
+++ api.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Jogos:
+    def __init__(self):
+        pass
+    def jogo_do_dia(self):
+        pass
+    def jogos_de_hoje(self):
+        pass
+    def jogos_de_amanha(self):
+        pass
+
+
